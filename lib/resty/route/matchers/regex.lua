a9acbc1b71b35a812b3abe8959c84e424dce99e5
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-10-06 19:34:46 +0300

    renamed c variable to m.

--- lib/resty/route/matchers/regex.lua
+++ lib/resty/route/matchers/regex.lua
@@ -2,12 +2,12 @@ local match = ngx.re.match
 local unpack = table.unpack or unpack
 local concat = table.concat
 return function(location, pattern)
-    local c = match(location, concat{ pattern, "$" }, "aijosu")
-    if c then
-        if c[1] then
-            return unpack(c)
+    local m = match(location, concat{ pattern, "$" }, "aijosu")
+    if m then
+        if m[1] then
+            return unpack(m)
         end
-        return c[0]
+        return m[0]
     end
     return nil
 end

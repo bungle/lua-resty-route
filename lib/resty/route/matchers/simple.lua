6494e12dc6c3d8878d580c7cb3e0ffe81ee36914
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-10-07 14:01:59 +0300

    Small improvements on simple matcher.

--- lib/resty/route/matchers/simple.lua
+++ lib/resty/route/matchers/simple.lua
@@ -4,34 +4,36 @@ local concat = table.concat
 local find = string.find
 local sub = string.sub
 local tonumber = tonumber
-local tostring = tostring
 return function(location, pattern)
-    local i, c, p = 1, {}, {}
+    local i, c, p, j = 1, {}, {}, 0
     local s = find(pattern, ":", 1, true)
     while s do
         p[#p+1] = sub(pattern, i, s - 1)
         local x = sub(pattern, s, s + 6)
         if x == ":number" then
             p[#p+1] = [[(\d+)]]
-            c[#c+1] = tonumber
-            s = s + 7
+            s, j = s + 7, j + 1
+            c[j] = tonumber
         elseif x == ":string" then
             p[#p+1] = [[([^/]+)]]
-            c[#c+1] = tostring
-            s = s + 7
+            s, j = s + 7, j + 1
+            c[j] = false
         end
         i = s
         s = find(pattern, ":", s + 1, true)
     end
-    if #c > 0 then
+    if j > 0 then
         p[#p+1] = sub(pattern, i)
         pattern = concat(p)
     end
+    print(pattern)
     local m = match(location, concat{ pattern, "$" }, "aijosu")
     if m then
         if m[1] then
-            for i, f in ipairs(c) do
-                m[i] = f(m[i])
+            for i = 1, j do
+                if c[i] then
+                    m[i] = c[i](m[i])
+                end
             end
             return unpack(m)
         end

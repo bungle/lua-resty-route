e3b9987fa387da7556c3946a331bf5b6511a495c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-06-15 14:54:49 +0300

    Fixed reqargs middleware.

--- lib/resty/route/middleware/reqargs.lua
+++ lib/resty/route/middleware/reqargs.lua
@@ -4,21 +4,16 @@ local pairs = pairs
 
 local function cleanup(self)
     local files = self.files
-    for i = 1, files.n, 1 do
-        local f = files[i]
-        remove(f.temp)
-        files[i] = nil
-    end
-    files.n = nil
-    for n, f in pairs(files) do
+    for _, f in pairs(files) do
         if f.n then
-            for i = 1, f.n, 1 do
-                remove(f[n][i].temp)
+            for i = 1, f.n do
+                remove(f[i].temp)
             end
         else
             remove(f.temp)
         end
     end
+    self.files = {}
 end
 
 return function(self)

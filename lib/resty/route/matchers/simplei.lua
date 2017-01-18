8b73bf9553958e7f9ac69250b8279fda546decce
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-01-19 01:41:27 +0200

    Small adjustments to simple matcher.

--- lib/resty/route/matchers/simplei.lua
+++ lib/resty/route/matchers/simplei.lua
@@ -3,28 +3,30 @@ local unpack   = table.unpack or unpack
 local concat   = table.concat
 local find     = string.find
 local sub      = string.sub
+local huge     = math.huge
 local tonumber = tonumber
 local unescape = ngx.unescape_uri
 return function(location, pattern)
-    local i, c, p, j = 1, {}, {}, 0
+    local i, c, p, j, n = 1, {}, {}, 0, 1
     local s = find(pattern, ":", 1, true)
     while s do
-        p[#p+1] = sub(pattern, i, s - 1)
+        p[n] = sub(pattern, i, s - 1)
+        n = n + 1
         local x = sub(pattern, s, s + 6)
         if x == ":number" then
-            p[#p+1] = [[(\d+)]]
-            s, j = s + 7, j + 1
+            p[n] = [[(\d+)]]
+            s, j, n = s + 7, j + 1, n + 1
             c[j] = tonumber
         elseif x == ":string" then
-            p[#p+1] = [[([^/]+)]]
-            s, j = s + 7, j + 1
+            p[n] = [[([^/]+)]]
+            s, j, n = s + 7, j + 1, n + 1
             c[j] = unescape
         end
         i = s
         s = find(pattern, ":", s + 1, true)
     end
     if j > 0 then
-        p[#p+1] = sub(pattern, i)
+        p[n] = sub(pattern, i)
         pattern = concat(p)
     end
     local m = match(location, concat{ pattern, "$" }, "aijosu")
@@ -32,6 +34,7 @@ return function(location, pattern)
         if m[1] then
             for i = 1, j do
                 m[i] = c[i](m[i])
+                if m[i] == huge then return nil end
             end
             return unpack(m)
         end
